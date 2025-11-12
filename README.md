55c094ed537dca1db563f01137886496b29f835f
Enrico <enricokiin@gmail.com>
2025-11-12 16:12:57 +0200

    Vähendasin ebavajaliku koodi ning kustutasin paar vanat faili, mis enam polnud kasutuses. Tekitasin kausta testimine, et saaks erinevate katseta tulemusi sinna jätta vajadusel.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Proge_1_projekt
+
+Autorid: Enrico Kiin ja Tima Matias Juksaar
+
+## Eesmärgid
+Luua scraper ostukorvid.ee jaoks, mis käib läbi meie poolt antud kategooriad ning paneb toodete info mingisse tabeltöötlus programmi, et info oleks paremini hallatav.
+Ideaalis tahame ka info veebilehele panna, et saaks teiste tudengitega paremini jagada.
+
+## Kasutamine
+Kui kasutate seda scraperit, siis ärge pommitage ostukorvid.ee lehekülgi liiga paljude requestidega. Time.sleeid on pandud mõttega sinna, et lehte mitte ülekoormata ning lisaks, et scraper töötaks. Ilma nendeta see throttleib teid ja scraper ei tööta vähemalt 1h aega(testimist saadud teada). Context ja küpsise uuendused on, et leht ei saaks aru, et on scraper, aga IP on sama, et seda scraperit ei saa liiga mitu korda samalt IPlt järjest kasutada ning pole ka vaja, sest info ei uuene nii tihti.
+
+Alguses tehke "pip install requests beautifulsoup4 playwright" ja "playwright install". Veenduge, et teete seda seal, kus teie Pythoni failid on, mida plaanite kasutada. GitHubist saades pole vaja (vist?).
+
+Scraper töötab üldjuhul ilma vigadeta, aga vahest saab töö lõpu poole throttled, et paar viimast toodet võib kaduma minna. Kui juba saad throttled, siis ootab paar tundi ja proovi uuesti.
--- scraper_real.py
+++ scraper_real.py
@@ -1,7 +1,7 @@
 """ 
 Töö autorid: Enrico Kiin ja Tima Mattias Juksaar
 See on scraper ostukorvid.ee lehekülje jaoks, millega saab koguda infot alkohoolitoodete kohta ja selle mõte on leida kõige suurema etanooli sisaldusega toode kõige väiksema hinna eest.
-Alguses tee pip install requests beautifulsoup4 playwright terminalis, kuna need pole standard teegid.
+Alguses tee "pip install requests beautifulsoup4 playwright" terminalis, kuna need pole standard teegid. Lisaks "playwright install"
 """
 
 
@@ -22,79 +22,75 @@ def brauser():
     """
     Brauser set-up, millega hakkame lehtedel käima ja scrapima.
     
-    Anname võimalikult palju resursse ja valime igakord juhusliku contexti väärtuse,
+    Valime igakord juhuslikud contexti väärtused,
     et ei näeks välja nagu sama inimene.
     Keelame piltide ja CSS laadimise, et vähendada aja ja resurssi kulu.
     """
     p = sync_playwright().start()
-    storage_state = "auth.json" if os.path.exists("auth.json") else None
+    küpsised = "auth.json" if os.path.exists("auth.json") else None
     suvaline_locale_timzone_dict = random.choice(locale_timezone_list)
 
     browser = p.chromium.launch(
         headless = True,
         args=[
-        "--disable-gpu",
-        "--disable-extensions",
-        "--no-sandbox",
-        "--disable-dev-shm-usage",
         "--blink-settings=imagesEnabled=false",
         "--window-size=1920,1080"
         ]
     )
     
     context = browser.new_context(
-        storage_state=storage_state,
+        storage_state=küpsised,
         user_agent= random.choice(user_agent_list),
         locale=suvaline_locale_timzone_dict["locale"],
         timezone_id=suvaline_locale_timzone_dict["timezone"],
         viewport={"width": 1920, "height": 1080},
-        bypass_csp=True,
     )
 
     page = context.new_page()
-    page.set_default_timeout(15000) # ms
+    page.set_default_timeout(25000) # ms
     
+    # Sain siit leheküljelt baas versiooni ja muutsin seda vastavalt vajdusele
+    # https://www.zenrows.com/blog/blocking-resources-in-playwright#prerequisites
     page.route("**/*", 
                lambda route, request: route.abort()
                    if request.resource_type in ["image","font","stylesheet"] 
                    else route.continue_()
     )
 
-    return p, context, page
+    return p, page
     
 
-def uued_küpsised(page, context):
+def uued_küpsised(page):
     """
     Loon ja uuendan küpsiseid, et näha rohkem välja nagu inimene mitte bot.
     """
     
     print("Küpsiste uuendamine")
     page.goto("https://ostukorvid.ee", wait_until="networkidle")
-    context.storage_state(path="auth.json")
+    page.context.storage_state(path="auth.json")
     print("Valmis")
 
 
-def värskenda_context_küpsised(p, vana_context):
+def värskenda_context_küpsised(vana_context):
     """
     Contexti värskendamiseks, et vähendada lehe throttle võimalusi.
     """
 
     vana_context.close()
     browser = vana_context.browser
-    storage_state = "auth.json" if os.path.exists("auth.json") else None
+    küpsised = "auth.json" if os.path.exists("auth.json") else None
     suvaline_locale_timzone_dict = random.choice(locale_timezone_list)
     
     context = browser.new_context(
-        storage_state=storage_state,
+        storage_state=küpsised,
         user_agent= random.choice(user_agent_list),
         locale=suvaline_locale_timzone_dict["locale"],
         timezone_id=suvaline_locale_timzone_dict["timezone"],
         viewport={"width": 1920, "height": 1080},
-        bypass_csp=True,
     )
 
     page = context.new_page()
-    page.set_default_timeout(15000) # ms
+    page.set_default_timeout(25000) # ms
     
     page.route(
         "**/*", 
@@ -103,9 +99,9 @@ def värskenda_context_küpsised(p, vana_context):
             else route.continue_()
     )
     
-    uued_küpsised(page, context)
+    uued_küpsised(page)
 
-    return context, page
+    return page
 
 
 def safe_goto(page, leht, proove=3, timeout=15000):
@@ -116,8 +112,6 @@ def safe_goto(page, leht, proove=3, timeout=15000):
     for katse in range(proove):
         try:
             page.goto(leht, wait_until="networkidle", timeout=timeout)
-            if page.locator(".col-span-2.mt-2").count() > 0:
-                return True
             page.wait_for_selector(".col-span-2.mt-2", timeout=timeout)
             return True
         except:
@@ -135,6 +129,7 @@ def sitemap_info(sihtmärgid):
     sitemap_info = requests.get(sitemap_url)
     sitemap_soup = BeautifulSoup(sitemap_info.content, "xml")
     mustad_kategooriad = [loc.text for loc in sitemap_soup.find_all("loc")]
+    
     # Eemaldab alamkategooriad, et vähem asju otsida
     puhtad_kategooriad = [url for url in mustad_kategooriad if "?tag=" not in url] 
 
@@ -214,7 +209,7 @@ def kasulik_info(page, leht):
     return kasulikud_info_kaardid
 
 
-def toote_scraper(p, page, jär, kategooria_nimi):
+def toote_scraper(page, jär, kategooria_nimi):
     """
     Leian kategoorias iga korrektse infoga toote kohta kõik info:
     nimi, maht, protsent, etanooli kogus, etanool euro kohta, poe nimi, hind, millal uuendati.
@@ -226,33 +221,34 @@ def toote_scraper(p, page, jär, kategooria_nimi):
     mitmes_toode = 0
     read = []
     
-    toote_protsent_muster = re.compile(r"(\d+(?:[.,]\d+)?)\s*%", re.IGNORECASE)
+    toote_protsent_muster = re.compile(r"(\d+(?:\s*[.,]\s*\d+)?)\s*%", re.IGNORECASE)
     
     toote_maht_muster = re.compile(r"""
-                                    (\d+(?:[.,]\d+)?)  # ühiku väärtus nt 500
+                                    (\d+(?:\s*[.,]\s*\d+)?)  # ühiku väärtus nt 500
                                     \s*
-                                    (ml|cl|l)          # ühik
+                                    (ml|cl|l)                # ühik
                                     """ 
                                     , re.VERBOSE | re.IGNORECASE)
     
     
-    hind_muster = re.compile(r"(\d+(?:[.,]\d+)?)\s*€", re.IGNORECASE)
+    hind_muster = re.compile(r"(\d+(?:\s*[.,]\s*\d+)?)\s*€", re.IGNORECASE)
     
-    viimati_uuendatud_muster = re.compile(r"""(?:(\d+)[\s\u00A0\u202F]*)?           # väärtus
-                                                    ([\wäöüõ]+)                     # ühik
-                                                    (?:[\s\u00A0\u202F]+aega)?      # \u00A0 - nbsp \u202F - nbsp narrrow. Siin vaja millegipärast, muidu annab None vahest
-                                                    [\s\u00A0\u202F]*tagasi""",
-                                                    re.UNICODE | re.VERBOSE | re.IGNORECASE)
+    viimati_uuendatud_muster = re.compile(r"""
+                                            (?:(\d+)[\s\u00A0\u202F]*)?     # väärtus
+                                            ([\wäöüõ]+)                     # ühik
+                                            (?:[\s\u00A0\u202F]+aega)?      # \u00A0 - nbsp \u202F - nbsp narrrow. Siin vaja millegipärast, muidu annab None vahest
+                                            [\s\u00A0\u202F]*tagasi""",
+                                            re.UNICODE | re.VERBOSE | re.IGNORECASE)
     for el in jär:
         if mitmes_toode % 20 == 0:
             time.sleep(random.uniform(3.0, 6.0))
         if mitmes_toode != 0 and mitmes_toode % 100 == 0:
-            context, page = värskenda_context_küpsised(p, page.context)
+            page = värskenda_context_küpsised(page.context)
             print("Scraper värskendatud")
         
 
         toode_el = el[0].strip()
-        toode = toode_el.strip().lower()
+        toode = toode_el.lower()
         toote_maht_tekst = el[1]
         toote_link = el[2]
         
@@ -262,7 +258,7 @@ def toote_scraper(p, page, jär, kategooria_nimi):
             print(f"PROTSENT ERROR: {toode}")
             continue
 
-        toote_protsent = float(protsent_tekst.group(1).replace(',', '.'))
+        toote_protsent = float(protsent_tekst.group(1).replace(',', '.').replace(" ", ""))
         if toote_protsent == 0.0: # Mõnel alko tootel pandud 0.0, et näidata alkovaba
             print(f"Alkovaba err: {toode}")
             continue
@@ -273,7 +269,7 @@ def toote_scraper(p, page, jär, kategooria_nimi):
         if not maht_tekst:
             print(f"MAHU VIGA: {toode}")
             continue
-        toote_maht = float(maht_tekst.group(1).replace(',', '.'))
+        toote_maht = float(maht_tekst.group(1).replace(',', '.').replace(" ", ""))
         
         mahu_ühik = maht_tekst.group(2)
 
@@ -320,7 +316,7 @@ def toote_scraper(p, page, jär, kategooria_nimi):
             hind_el = pood.select_one("span.text-xl.font-bold")
             if hind_el:
                 hind_tekst = hind_el.text.strip()
-                hind = float(hind_muster.search(hind_tekst).group(1).replace(',', '.'))
+                hind = float(hind_muster.search(hind_tekst).group(1).replace(',', '.').replace(" ", ""))
             else:
                 hind = None
 
@@ -345,8 +341,8 @@ def toote_scraper(p, page, jär, kategooria_nimi):
                         aja_väärtus = int(viimati_uuendatud_tekst.group(1))
                     else:
                         aja_väärtus = 1 # Kuna tekstis on "uuendatud: tund/minut/päev aega tagasi"
-                    ühiku_väärtus = viimati_uuendatud_tekst.group(2)
-                    sekundi_väärtus = ühikud.get(ühiku_väärtus)
+                    ühik_aeg = viimati_uuendatud_tekst.group(2)
+                    sekundi_väärtus = ühikud.get(ühik_aeg)
                     if not sekundi_väärtus:
                         aeg_ümar = "None_sekundi_väärtus"
                     else:
@@ -387,7 +383,7 @@ def toote_scraper(p, page, jär, kategooria_nimi):
     for i in range(2):
         print()
 
-    return page.context, page
+    return page
 
 
 def main():
@@ -396,17 +392,17 @@ def main():
     mis sisaldab kõige rohkem etanooli.
     Tagastan info csv failidena.
     """
-    p, context, page = brauser()
-    uued_küpsised(page, context)
+    p, page = brauser()
+    uued_küpsised(page)
     
     scrapeime = ["olu", "viin", "siider", "vein"]
     kategooriad = sitemap_info(scrapeime)
 
     for kategooria_nimi, leht in kategooriad:
         tooted = kasulik_info(page, leht)
-        context, page = toote_scraper(p, page, tooted, kategooria_nimi)
+        page = toote_scraper(page, tooted, kategooria_nimi)
     
-    context.close()
+    page.context.close()
     p.stop()
 
 if __name__=="__main__":
